16e40b40b02e51d99647029f1ff918a053a53b1d
scorlibrian <william.carducci+github@gmail.com>
2015-12-13 21:43:57 -0500

    Added Bezier Curve

---
+++ Assets/MathUtil.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class MathUtil
+{
+	public static Vector3 BezierInterpolation(float t, params Vector3[] points)
+	{
+		if(points.Length == 0)
+		{
+			return Vector3.zero;
+		}
+		else if(points.Length == 1)
+		{
+			return points[0];
+		}
+		else
+		{
+			int elementsToCopy = points.Length - 1;
+			Vector3[] points1 = new Vector3[elementsToCopy];
+			Vector3[] points2 = new Vector3[elementsToCopy];
+			Array.Copy(points, 1, points1, 0, elementsToCopy);
+			Array.Copy(points, 0, points2, 0, elementsToCopy);
+
+			Vector3 start = BezierInterpolation(t, points1);
+			Vector3 end = BezierInterpolation(t, points2);
+
+			return Vector3.Lerp(start, end, t);
+		}
+	}
+}
